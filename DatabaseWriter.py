29f29771bc63f15cfc8545182ec4fb6326fd6265
JonathanReimerBerg <86084308+JonathanReimerBerg@users.noreply.github.com>
2022-04-17 10:46:47 -0400

    Update DatabaseWriter.py

--- DatabaseWriter.py
+++ DatabaseWriter.py
@@ -9,19 +9,19 @@ groups = []
 with open('eBird_Taxonomy_v2021.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            if ("(" in row[3]) or (" x " in row[3]) or ("/" in row[3]) or ("sp." in row[3]):
+            if ("(" in row[3]) or (" x " in row[3]) or ("/" in row[3]) or ("sp." in row[3]):  #removes non-valid birds, like subspecies
                 continue
-            if row[6] == "FAMILY":
+            if row[6] == "FAMILY":   #don't count first row
                 continue
-            names.append(row[3].replace("'", "''"))
+            names.append(row[3].replace("'", "''"))  #for SQL grammer
             sci_names.append(row[4])
             group = row[6]
-            groups.append(group[group.find("(")+1:group.find(")")].replace("'", "''"))
+            groups.append(group[group.find("(")+1:group.find(")")].replace("'", "''")) #just add what is in parenthesis
 
 connection = sqlite3.connect("BirdingDatabase.db")
 crsr = connection.cursor()
 
-command = "DROP TABLE IF exists ALL_BIRDS;"
+command = "DROP TABLE IF exists ALL_BIRDS;"   #writing command to create table ALL_BIRDS in database
 crsr.execute(command)
 connection.commit()
 
@@ -45,14 +45,14 @@ command = "INSERT INTO ALL_BIRDS(name, scientific_name, type, seen, in_2018, in_
 
 for i in range(0, len(names)):
     command += '\n'
-    command += ("    ('" + names[i] + "', '" + sci_names[i] + "', '" + groups[i] + "', 0, 0, 0, 0, 0, 0),")
+    command += ("    ('" + names[i] + "', '" + sci_names[i] + "', '" + groups[i] + "', 0, 0, 0, 0, 0, 0),") # 0 -> not seen, 1 -> seen
 
-command = command[:-1]
+command = command[:-1] #remove final comma
 
 crsr.execute(command)
 connection.commit()
 
-command = "DROP TABLE IF exists COUNTIES;"
+command = "DROP TABLE IF exists COUNTIES;"  #create county table
 crsr.execute(command)
 connection.commit()
 
